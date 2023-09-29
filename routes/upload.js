0d3775f86942b456e9a27057f3a1e658b820b7ee
Akinyemi <hiibeekayvibe@gmail.com>
2023-09-29 12:34:36 +0100

    chaged route def

--- routes/upload.js
+++ routes/upload.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const uploadController = require("../controllers/uploadController");
+const uploadController = require("../controllers/upload");
 const multer = require("multer");
 const {
   uploadVideo,
